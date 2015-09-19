36d4cf6afd6b678a9920c9343a3b90b74690dddc
Ali Rahbar <junk0xc0de@gmail.com>
2015-09-19 16:58:48 +0430

    [sfpool.h] header guard and basics

---
+++ sfpool.h
@@ -0,0 +1,26 @@
+/*
+ * Simple Fast Memory Pool
+ */
+
+#ifndef SFPOOL_H
+#define SFPOOL_H
+
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+struct sfpool_page
+{
+};
+
+struct sfpool
+{
+
+};
+
+#endif /* SFPOOL_H_ */
