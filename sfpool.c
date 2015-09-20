9f87b5833dbc91b6109930d08f12d3a5dbac18a5
Ali Rahbar <junk0xc0de@gmail.com>
2015-09-20 17:03:22 +0430

    add_page() added

--- sfpool.c
+++ sfpool.c
@@ -42,11 +42,63 @@ struct sfpool* sfpool_create (size_t item_size,size_t item_count)
     
     pool->item_size = round_size(item_size);
 
+    /* check for previous page and find out how big this page should be ? */
+
+    if(pool->pages != NULL) /* so there are already some pages in the pool */
+    {
+        
+    }
+
     return pool;
 }
 
-static int add_page (struct sfpool* pool)
+static int add_page (struct sfpool* pool,size_t item_count)
 {
-    size_t raw_size = ((WORD_SIZE + pool->item_size) * pool->item_count) + sizeof(struct sfpool_page);
-    unsigned char* raw_data = malloc(raw_size);
+    size_t raw_size = ((WORD_SIZE + pool->item_size) * item_count) + sizeof(struct sfpool_page);
+    struct sfpool_page* page = (struct sfpool_page*) malloc(raw_size);
+
+    if(page == NULL)
+    {
+        return 1;
+    }
+
+    /* initializing the new page */
+
+    page->pool = pool;
+
+    /* link the last page and this page together */
+
+    page->prev = pool->pages;
+    page->next = NULL;
+
+    if(pool->pages != NULL)
+    {
+        pool->pages->next = page;
+    }
+    if(pool->working_page == NULL)
+    {
+        pool->working_page = page;
+    }
+
+    pool->pages = page;
+    pool->item_count += item_count;
+    pool->page_count++;
+
+    /* generate free items */
+
+    unsigned char* start_address = (unsigned char*) &page->items;
+    size_t* header = (size_t*) start_address;
+    size_t* header_next = NULL;
+    size_t distance = (WORD_SIZE + pool->item_size) / WORD_SIZE;
+
+    for(size_t i = 0;i < (item_count - 1);i++)
+    {
+        header_next = header + distance;
+        *header = (size_t) header_next;
+        header = header_next;
+    }
+
+    *header = 0x0;
+
+    return 0;
 }
