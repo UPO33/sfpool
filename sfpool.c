b1783e1f083b317ec63e4b69961cd40b99093b7e
Ali Rahbar <junk0xc0de@gmail.com>
2015-09-20 10:48:36 +0430

    address aligner function added

--- sfpool.c
+++ sfpool.c
@@ -1,1 +1,21 @@
 #include "sfpool.h"
+
+#define WORD_SIZE ((size_t) - 1)
+
+inline size_t round_size (size_t size,size_t align)
+{
+    if(size < align)
+    {
+        size = align;
+        return size;
+    }
+
+    size_t mod = size % align;
+
+    if(mod != 0)
+    {
+        size += align - mod;
+    }
+    
+	return size;
+}
